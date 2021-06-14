c1964b343803ff4ffaa4050a178199f933235427
Thinh Nguyen <thinh.nguyen@dell.com>
2021-06-14 20:19:42 +0000

    Added deployment script

--- ci/main/tasks/deploy.sh
+++ ci/main/tasks/deploy.sh
@@ -3,8 +3,8 @@ set -e -x
 
 echo "Do nothing"
 mkdir ~/.kube
-echo $KUBECONFIG_FILE > ~/.kube/config
+cp qre-secrets/kube_config.yaml ~/.kube/config
 
-# cd qiskit-runtime-emulator/server/deployments
-# source test.sh
-# envsubst < ./qre.yaml | kubectl apply -f -
+cd qiskit-runtime-emulator/server/deployments
+source test.sh
+envsubst < ./qre.yaml | kubectl apply -f -

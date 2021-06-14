904c9a563655eafc0a823062b6a42afe9f66a555
Thinh Nguyen <thinh.nguyen@dell.com>
2021-06-14 23:39:58 +0000

    Added remove qre test env

--- ci/main/tasks/deploy.sh
+++ ci/main/tasks/deploy.sh
@@ -6,6 +6,7 @@ cp qre-secrets/kube_config.yaml ~/.kube/config
 
 cd qiskit-runtime-emulator/server/deployments
 source test.sh
+envsubst < ./qre.yaml | kubectl delete -f -
 envsubst < ./qre.yaml | kubectl apply -f -
 
 kubectl wait --for=condition=Ready pods --all -n $(echo $QRE_NS)

62505e97dd085dbc13947278e344cd128c0f86b9
Seth Rothschild <Seth.Rothschild@emc.com>
2021-06-18 13:07:20 -0400

    Push to IBM repo

--- ci/main/tasks/release.sh
+++ ci/main/tasks/release.sh
@@ -17,6 +17,12 @@ git commit -m "[ci skip] bump version"
 cd ../qiskit-runtime-emulator
 git tag -a $newVersion -m "CICD Build Tag"
 
+# cd ..
+# cp -r qiskit-runtime-emulator/ ibm-repo/
 
+# cd ibm-repo
+# git add . 
+# git commit -m "build $newVersion"
+# git tag -a $newVersion -m "CICD Build Tag"
 
 

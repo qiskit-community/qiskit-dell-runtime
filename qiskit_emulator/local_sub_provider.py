9a06c504e9e8e26f2aa8baf92fb78947333d91b0
Victor Fong <v.fong@dell.com>
2021-06-28 11:07:31 -0400

    Added backend support for multiple backends

---
+++ qiskit_emulator/local_sub_provider.py
@@ -0,0 +1,39 @@
+from qiskit import Aer
+from qiskit_ionq import IonQProvider
+from qiskit.providers.exceptions import QiskitBackendNotFoundError
+# from qiskit import IBMQ
+
+from .emulator_backend import EmulatorBackend
+
+class LocalSubProviderManager():
+    def __init__(self, provider):
+        # self.sub_providers = [Aer, IonQProvider(), IBMQ.get_provider()]
+        # self.sub_providers = [Aer, IonQProvider()]
+        self._provider = provider
+        self._init_backends()
+            
+    def _init_backends(self):
+        self._backends = [Aer.get_backend('aer_simulator')]
+        self._backends.append(EmulatorBackend(self._provider))
+        self._backends += IonQProvider().backends()
+
+        # for sub_provider in self.sub_providers:
+            # self._backends += sub_provider.backends()
+        
+        self._backends_by_name = {}
+        for backend in self._backends:
+            self._backends_by_name[backend.name()] = backend
+
+
+    def backends(self):
+        return self._backends
+
+    def get_backend(self, name=None, **kwargs):
+        if name not in self._backends_by_name:
+            raise QiskitBackendNotFoundError("No backend matches criteria.")
+        else:
+            return self._backends_by_name[name]
+
+
+
+    

51e4b2164c013aad3656b9c32827ba4422595411
Ben Santaus <ben_santaus@dell.com>
2021-07-26 20:19:58 +0000

    wip msg token

--- server/executor/user_messenger_client.py
+++ server/executor/user_messenger_client.py
@@ -10,6 +10,9 @@ class RemoteUserMessengerClient(UserMessenger):
     def __init__(self):
         self.host = os.environ['ORCH_HOST']
         self.job_id = os.environ['JOB_ID']
+        self._session = requests.Session()
+        url = urljoin(self.host, f'/register_messenger/{self.job_id}')
+        res = self._session.get(url, data={'token': os.environ["MESSAGE_TOKEN"]})
 
     def publish(
             self,
@@ -20,7 +23,7 @@ class RemoteUserMessengerClient(UserMessenger):
     
         str_msg = json.dumps({"final": final, "message": message}, cls=encoder)
         url = urljoin(self.host, f'job/{self.job_id}/message')
-        req = requests.post(url, str_msg)
+        req = self._session.post(url, str_msg)
         if req.status_code != 200:
             raise (f'Error POST {url}: {req.status_code}')
 

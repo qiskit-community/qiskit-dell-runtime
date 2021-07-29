ea8346d7d0c9c47ae0c6d400eedcbf111c49ba98
Ben Santaus <ben_santaus@dell.com>
2021-07-29 15:29:38 +0000

    update ci and test script

--- server/deployments/test.sh
+++ server/deployments/test.sh
@@ -1,6 +1,21 @@
 export QRE_NS=qre-test
-export SESSION_SECRET_KEY="g;pqj235hkngfava@^nffKN23"
-export SESSION_TYPE="filesystem"
+
+export DOCKER_REPO=harbor.dell.com/dojo-harbor
+
+export DB_IMAGE=qre/mysql:5.6
+export DB_TYPE=mysql
+export DB_TYPE=${DB_TYPE,,}
+export DB_UPPER=${DB_TYPE^^}
+export ${DB_TYPE^^}_DATABASE=qre
+export ${DB_TYPE^^}_USER=qre
+export DB_PORT=3306
+
+export KUBE_LOCATION=oro-sandbox-small1.k8s.cec.lab.emc.com
+export SERVER_URL="http://$QRE_NS.$KUBE_LOCATION"
+
 export SSO_TOKEN_URL="https://appsso.login.scfd.isus.emc.com/oauth/token"
 export SSO_AUTH_URL="https://appsso.login.scfd.isus.emc.com/oauth/authorize"
 export SSO_INFO_URL="https://appsso.login.scfd.isus.emc.com/userinfo"
+
+export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+export QRE_CERTS_DIR=/home/geoff/workspace/qre-secrets
